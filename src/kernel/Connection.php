c2ea08022f6162f2889d105a1a21a27366c5a234
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-08-06 20:40:36 -0300

    Namespaces added

--- src/kernel/Connection.php
+++ src/kernel/Connection.php
@@ -1,1 +1,11 @@
 <?php
+
+/**
+ * MBHFramework
+ *
+ * @link      https://github.com/MBHFramework/mbh-framework
+ * @copyright Copyright (c) 2017 Ulises Jeremias Cornejo Fandos
+ * @license   https://github.com/MBHFramework/mbh-framework/blob/master/LICENSE (MIT License)
+ */
+ 
+namespace Slim;
