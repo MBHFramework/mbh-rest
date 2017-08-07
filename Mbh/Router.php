d887b00968bac76abda5c9dcf0779e609ce3064b
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-08-07 19:20:39 -0300

    Router

--- Mbh/Router.php
+++ Mbh/Router.php
@@ -9,3 +9,35 @@
  */
 
 namespace Mbh;
+
+# Security
+defined('INDEX_DIR') or exit(APP_NAME . 'software says .i.');
+
+/**
+ * created by Ulises Jeremias Cornejo Fandos
+ */
+final class Router
+{
+    protected $request_uri;
+    protected $routes;
+
+    final public function __construct(string $request_uri = $_SERVER['REQUEST_URI'])
+    {
+          $this->setRequestUri($request_uri);
+          $this->routes = [];
+    }
+
+    public function setRequestUri($request_uri)
+  	{
+          if (strpos($request_uri, self::GET_PARAMS_DELIMITER))
+          {
+          	$request_uri = strstr($request_uri, self::GET_PARAMS_DELIMITER, true);
+          }
+          $this->requestUri = $request_uri;
+  	}
+
+  	public function getRequestUri()
+  	{
+          return $this->requestUri;
+  	}
+}
