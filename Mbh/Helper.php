d252f4f2945b56411135a021c45e52a6c15c7d6f
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-08-07 18:33:30 -0300

    Helper class added

--- Mbh/Helper.php
+++ Mbh/Helper.php
@@ -18,5 +18,30 @@ defined('INDEX_DIR') or exit(APP_NAME . 'software says .i.');
  */
 final class Helper
 {
+    /**
+      * const ROUTE: Constant that indicates the route according to where the loader is called, from the REST API or from the Application
+      *
+      */
+    const ROUTE = IS_API ? '../Mbh/Helpers/' : 'Mbh/Helpers/';
 
+    /**
+      * Static load a helper hosted in the kernel folder helpers for later use
+      *
+      * @param string $helper: Helper name
+      *
+      * @return void
+      */
+    final static public function load(string $helper, Twig_Environment $object = null) {
+      $helper = ucwords($helper);
+      $file = self::ROUTE . $helper . '.php';
+      if(file_exists($file)) {
+        include_once($file);
+        # Twig integration
+        if($object instanceof Twig_Environment) {
+          $object->addExtension(new $helper());
+        }
+      } else {
+        trigger_error('The helper ' . $helper . ' does not exist in the helpers library.', E_USER_ERROR);
+      }
+    }
 }
