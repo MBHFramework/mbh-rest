f977224b3576951f16da6b0a56f63e3d61783e60
ulises-jeremias <ulisescf.24@gmail.com>
2017-08-07 16:56:17 -0300

    index.php update

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ require 'vendor/autoload.php';
 require 'Mbh/config/config.php';
 require 'Mbh/App.php';
 
-
 \Mbh\App::registerAutoload();
 
 $app = new \Mbh\App();
 
-
+/**
+  * Specific routes should be defined here
+  */
 
 $app->run();
