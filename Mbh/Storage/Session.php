4ef489f85862d4557486f0a23f93c4bb0cafbeed
ulises-jeremias <ulisescf.24@gmail.com>
2017-08-08 00:43:22 -0300

    Beautify

--- Mbh/Storage/Session.php
+++ Mbh/Storage/Session.php
@@ -18,73 +18,73 @@ defined('INDEX_DIR') or exit(APP_NAME . 'software says .i.');
  */
 final class Session
 {
-  final public function __construct(array $settings = SESSION['CONF'])
-  {
-      if(!headers_sent()) {
-        session_start($settings);
-      }
-  }
+    final public function __construct(array $settings = SESSION['CONF'])
+    {
+        if (!headers_sent()) {
+            session_start($settings);
+        }
+    }
 
-  final public function set($key, $value)
-  {
-      $_SESSION[$key] = $value;
-  }
+    final public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
 
-  final public function setFlash($identifier, $message)
-  {
-      $_SESSION[$identifier] = $message;
-  }
+    final public function setFlash($identifier, $message)
+    {
+        $_SESSION[$identifier] = $message;
+    }
 
-  final public function getFlash($identifier)
-  {
-      if(array_key_exists($identifier, $_SESSION)) {
-          $keep = $_SESSION[$identifier];
-          $this->delete($identifier);
-          return $keep;
-      }
-  }
+    final public function getFlash($identifier)
+    {
+        if (array_key_exists($identifier, $_SESSION)) {
+            $keep = $_SESSION[$identifier];
+            $this->delete($identifier);
+            return $keep;
+        }
+    }
 
-  final public function get($key)
-  {
-      if($this->has($key)) {
-          return $_SESSION[$key];
-      }
-  }
+    final public function get($key)
+    {
+        if ($this->has($key)) {
+            return $_SESSION[$key];
+        }
+    }
 
-  final public function has($key)
-  {
-     return (array_key_exists($key, $_SESSION));
-  }
+    final public function has($key)
+    {
+        return (array_key_exists($key, $_SESSION));
+    }
 
-  final public function delete($key)
-  {
-      if($this->has($key)) {
-          unset($_SESSION[$key]);
-      }
-  }
+    final public function delete($key)
+    {
+        if ($this->has($key)) {
+            unset($_SESSION[$key]);
+        }
+    }
 
-  final public function destroy()
-  {
-      session_destroy();
-  }
+    final public function destroy()
+    {
+        session_destroy();
+    }
 
-  final public function offsetGet($offset)
-  {
-      return $this->get($offset);
-  }
+    final public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
 
-  final public function offsetSet($offset, $value)
-  {
-      $this->set($offset, $value);
-  }
+    final public function offsetSet($offset, $value)
+    {
+        $this->set($offset, $value);
+    }
 
-  final public function offsetExists($offset) {
-      return $this->has($offset);
-  }
+    final public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
 
-  final public function offsetUnset($offset) {
-      $this->delete($offset);
-  }
-
-
+    final public function offsetUnset($offset)
+    {
+        $this->delete($offset);
+    }
 }
