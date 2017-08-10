d7a83bdd1229c6a193c637028b64af84507c3400
ulises-jeremias <ulisescf.24@gmail.com>
2017-08-10 18:47:13 -0300

    Api working

--- api/index.php
+++ api/index.php
@@ -1,1 +1,19 @@
 <?php
+
+$root = __DIR__ . '/..';
+
+require "${root}/vendor/autoload.php";
+require "${root}/Mbh/Settings/config.php";
+require "${root}/Mbh/App.php";
+
+use \Mbh\App;
+
+App::registerAutoload();
+
+$app = new App();
+
+$app->any('/api/', function() {
+    return "MBHFramework working!";
+});
+
+$app->run();
